53234e401cf7e61bf6fc1d8c93db4c37949876b0
Meeri <meeri.mariella@gmail.com>
2024-12-11 18:58:05 +0200

    Removed unnecessary code

--- client/src/components/dashboard/ButtonSection.tsx
+++ client/src/components/dashboard/ButtonSection.tsx
@@ -30,21 +30,3 @@ const ButtonSection: React.FC<ButtonSectionProps> = (props) => {
 };
 
 export default ButtonSection;
-
-// const ButtonSection = (props) => {
-
-//     if (props.area != null) {
-//         return (
-//             <div className="button-section">
-//                 <button className="button" onClick={console.log("New")}>Report new trees</button>
-//                 <button className="button" onClick={console.log("Growth")}>Report growth</button>
-//             </div>
-//         )
-//     } else {
-//         return (
-//             <h2>Please choose area</h2>
-//         )
-//     }
-// }
-
-// export default ButtonSection
